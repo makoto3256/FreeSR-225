60bb9bdf80e0197dae0181ff82732beb735846e7
YinCQL <71113782+YinCQL@users.noreply.github.com>
2024-05-11 14:46:10 +0800

    Support 2.2.51

---
+++ FreeSR.Dispatch/Handlers/QueryGatewayHandler.cs
@@ -0,0 +1,39 @@
+﻿namespace FreeSR.Dispatch.Handlers
+{
+    using Ceen;
+    using FreeSR.Dispatch.Util;
+    using FreeSR.Proto;
+    using System.Threading.Tasks;
+
+    internal class QueryGatewayHandler : IHttpModule
+    {
+        public async Task<bool> HandleAsync(IHttpContext context)
+        {
+            context.Response.StatusCode = HttpStatusCode.OK;
+            context.Response.ContentType = "text/plain";
+            await context.Response.WriteAllAsync(Convert.ToBase64String(ProtobufUtil.Serialize(new Gateserver
+            {
+                Retcode = 0,
+                Msg = "OK",
+                Ip = "127.0.0.1",
+                //RegionName = "FreeSR",
+                Port = 23301,
+                Pdpbjhfgnjk = true,
+                Bipcmeeljhj = true,
+                Hecpclndaac = true,
+                Nlfkefmfige = true,
+                Oigmgpfnloj = true,
+                Pnnionnkbnn = true,
+                UseTcp = true,
+                //MdkResVersion = "5335706",
+                AssetBundleUrl = "https://autopatchos.starrails.com/asb/BetaLive/output_7037158_b67f5a6a68fb",
+                ExResourceUrl = "https://autopatchos.starrails.com/design_data/BetaLive/output_7033392_aaca9c1b456b",
+                IfixVersion = "0",
+                LuaUrl = "https://autopatchos.starrails.com/lua/BetaLive/output_7050564_f05a0f949b10",
+                LuaVersion = "7050564"
+            })));
+
+            return true;
+        }
+    }
+}
---
+++ FreeSR.Gateserver/Manager/Handlers/PlayerReqGroup.cs
@@ -0,0 +1,110 @@
+﻿namespace FreeSR.Gateserver.Manager.Handlers
+{
+    using FreeSR.Gateserver.Manager.Handlers.Core;
+    using FreeSR.Gateserver.Network;
+    using FreeSR.Proto;
+    using NLog;
+
+    using System;
+    using System.IO;
+    using System.Runtime.Serialization.Formatters.Binary;
+    using System.Text;
+    using Newtonsoft.Json;
+
+    internal static class PlayerReqGroup
+    {
+        private static readonly Logger s_log = LogManager.GetCurrentClassLogger();
+
+        [Handler(CmdType.CmdPlayerHeartBeatCsReq)]
+        public static void OnPlayerHeartBeatCsReq(NetSession session, int cmdId, object data)
+        {
+            var heartbeatReq = data as PlayerHeartBeatCsReq;
+
+            session.Send(CmdType.CmdPlayerHeartBeatScRsp, new PlayerHeartBeatScRsp
+            {
+                Retcode = 0,
+
+                DownloadData = new ClientDownloadData
+                {
+                    Version = 51,
+                    Time = DateTimeOffset.Now.ToUnixTimeMilliseconds(),
+                    Data = Convert.FromBase64String("G0x1YVMBGZMNChoKBAQICHhWAAAAAAAAAAAAAAAod0ABTkBDOlxVc2Vyc1x4ZW9uZGV2XERvY3VtZW50c1xnaXRcaGtycGdfYnVpbGRfc2VjdXJpdHlcamFkZV9zZWN1cml0eV9tb2R1bGUubHVhAAAAAAAAAAAAAQQfAAAAJABAAClAQAApgEAAKcBAAFYAAQAsgAABHUBBAKSAQQDkAEAA6cDBAekAwgHpQMIBrAAAASyAAAAfwEKFJABAAClAQAApgEAAKcBAAFYAAwAsgAABHUBBAKSAQQDkAEAA6cDBAekAwgHpQMIBrAAAASyAAAAfQEOFGQCAAA4AAAAEA0NTBAxVbml0eUVuZ2luZQQLR2FtZU9iamVjdAQFRmluZAQpVUlSb290L0Fib3ZlRGlhbG9nL0JldGFIaW50RGlhbG9nKENsb25lKQQXR2V0Q29tcG9uZW50SW5DaGlsZHJlbgQHdHlwZW9mBARSUEcEB0NsaWVudAQOTG9jYWxpemVkVGV4dAQFdGV4dBQqSmFkZVNSIGlzIGEgZnJlZSBhbmQgb3BlbiBzb3VyY2Ugc29mdHdhcmUEDFZlcnNpb25UZXh0FC5WaXNpdCBkaXNjb3JkLmdnL3JldmVyc2Vkcm9vbXMgZm9yIG1vcmUgaW5mbyEBAAAAAQAAAAAAHwAAAAEAAAABAAAAAQAAAAEAAAABAAAAAQAAAAEAAAABAAAAAQAAAAEAAAABAAAAAQAAAAEAAAABAAAAAQAAAAIAAAACAAAAAgAAAAIAAAACAAAAAgAAAAIAAAACAAAAAgAAAAIAAAACAAAAAgAAAAIAAAACAAAAAgAAAAIAAAAAAAAAAQAAAAVfRU5W")
+                },
+
+                ClientTimeMs = heartbeatReq.ClientTimeMs,
+                ServerTimeMs = (ulong)DateTimeOffset.Now.ToUnixTimeMilliseconds()
+            });
+        }
+
+        [Handler(CmdType.CmdGetHeroBasicTypeInfoCsReq)]
+        public static void OnGetHeroBasicTypeInfoCsReq(NetSession session, int cmdId, object _)
+        {
+            session.Send(CmdType.CmdGetHeroBasicTypeInfoScRsp, new GetHeroBasicTypeInfoScRsp
+            {
+                Retcode = 0,
+                Gender = Gender.GenderMan,
+                BasicTypeInfoLists ={
+                    new PlayerHeroBasicTypeInfo
+                    {
+                        BasicType = HeroBasicType.BoyWarrior,
+                        Rank = 1,
+                        SkillTreeLists = {}
+                    }
+                },
+                CurBasicType = HeroBasicType.BoyWarrior,
+            });
+        }
+
+        [Handler(CmdType.CmdGetBasicInfoCsReq)]
+        public static void OnGetBasicInfoCsReq(NetSession session, int cmdId, object _)
+        {
+            session.Send(CmdType.CmdGetBasicInfoScRsp, new GetBasicInfoScRsp
+            {
+                CurDay = 1,
+                ExchangeTimes = 0,
+                Retcode = 0,
+                NextRecoverTime = 2281337,
+                WeekCocoonFinishedCount = 0
+            });
+        }
+
+        [Handler(CmdType.CmdPlayerLoginCsReq)]
+        public static void OnPlayerLoginCsReq(NetSession session, int cmdId, object data)
+        {
+            var request = data as PlayerLoginCsReq;
+
+            session.Send(CmdType.CmdPlayerLoginScRsp, new PlayerLoginScRsp
+            {
+                Retcode = 0,
+                //IsNewPlayer = false,
+                LoginRandom = request.LoginRandom,
+                Stamina = 240,
+                ServerTimestampMs = (ulong)DateTimeOffset.Now.ToUnixTimeSeconds() * 1000,
+                BasicInfo = new PlayerBasicInfo
+                {
+                    Nickname = "xeondev",
+                    Level = 70,
+                    Exp = 0,
+                    Stamina = 100,
+                    Mcoin = 0,
+                    Hcoin = 0,
+                    Scoin = 0,
+                    WorldLevel = 6
+                }
+            });
+        }
+
+        [Handler(CmdType.CmdPlayerGetTokenCsReq)]
+        public static void OnPlayerGetTokenCsReq(NetSession session, int cmdId, object data)
+        {
+            session.Send(CmdType.CmdPlayerGetTokenScRsp, new PlayerGetTokenScRsp
+            {
+                Retcode = 0,
+                Uid = 1337,
+                //BlackInfo = null,
+                Msg = "OK",
+                SecretKeySeed = 0
+            });
+        }
+    }
+}
